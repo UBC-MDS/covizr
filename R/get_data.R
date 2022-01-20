ee4abfa1bbfed16ea33b3d36963419080d239cb6
Thomas Siu <thomas.siu@gmail.com>
2022-01-19 17:54:32 -0800

    doc: get_data documentation

---
+++ R/get_data.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param date_from Start date of the data range with format '%Y-%m-%d'. By default it represents 7 days prior to today's date
+#' @param date_to End date of data range with format '%Y-%m-%d'. By default it represents today's date
+#' @param location Charactor vector of target country names. By default it retrieves all countries
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' get_data(date_from='2022-01-01', date_to="2022-01-07", location=c('Canada'))
+get_data <- function(date_from, date_to, location) {
+
+}
